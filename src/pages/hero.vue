0c29255b6bcec762b3f1d80cce4ced81bb12972a
Wenish <Wenish@users.noreply.github.com>
2023-07-30 19:55:17 +0200

    update

--- src/pages/hero.vue
+++ src/pages/hero.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="text-4xl text-red-400">Hallo</div>
+    <div class="text-4xl text-red-400 p-4">Hallo</div>
+    <div>{{ data }}</div>
 </template>
 <script lang="ts" setup>
+const trpc = useTrpc()
+
+const data = await trpc.title.query()
 </script>
